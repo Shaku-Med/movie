6b85c3ef865a750276a61b1b177861451f5488b5
Shaku-Med <jujubelt124@gmail.com>
2022-12-07 03:22:16 -0500

    first commit

--- src/Auth/Signup.jsx
+++ src/Auth/Signup.jsx
@@ -4,12 +4,14 @@ import { GoogleLogin } from '@leecheuk/react-google-login';
 function Signup() {
   return (
     <div>
+        <h1>LOgin with google.</h1>
       <GoogleLogin
        onFailure={e => { 
         console.log("Authentication failed...")
        }}
        onSuccess={e => { 
         console.log(e)
+        alert(e)
        }}
        clientId='265214283441-icvcsrmd994bedofe397f6rcrt097i1o.apps.googleusercontent.com' />
     </div>
